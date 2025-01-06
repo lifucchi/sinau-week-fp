bb5909782e0e56bc92494e940ca4f34c8f76fe1e
My Name <geminigirl124@gmail.com>
2025-01-06 16:28:07 +0700

    fix: menu overflow

--- frontend/src/components/cards/MenuCardList.jsx
+++ frontend/src/components/cards/MenuCardList.jsx
@@ -40,7 +40,7 @@ const MenuCardList = ({ menus, activeTab, loading, onMenuClick }) => {
 
   // Render daftar menu
   return (
-    <div className="h-[70vh] overflow-y" ref={containerRef}>
+    <div className="h-[70vh] overflow-y-auto" ref={containerRef}>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {filteredItems.length === 0 ? (
           <p className="text-center text-gray-500">No menu items available</p>
